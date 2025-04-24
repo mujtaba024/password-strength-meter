34bc6b8a1e4f817935cef66e53af43412fa96c77
mujtaba024 <162799629+mujtaba024@users.noreply.github.com>
2025-04-24 09:31:38 +0500

    Update psm.py

--- psm.py
+++ psm.py
@@ -2,7 +2,7 @@ import re
 import streamlit as st
 
 #page styling
-st.set_page_config(page_title="Password Strength Meter By Asmara Faisal", page_icon="🔑", layout="centered")
+st.set_page_config(page_title="Password Strength checker By Asmara Faisal", page_icon="🔑", layout="centered")
 #custom css
 st.markdown("""
 <style>
